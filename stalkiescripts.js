21c97fd22890849b564cc98939e421cf9a5d9a5e
Stalkie <diogodigreja@gmail.com>
2026-05-04 22:14:27 +0100

    Add functions to manipulate HTML content and styles

---
+++ stalkiescripts.js
@@ -0,0 +1,34 @@
+// 1. Mudar o conteúdo HTML (Texto)
+function mudarConteudo() {
+    document.getElementById("demo").innerHTML = "Surprise! O texto foi alterado!";
+}
+
+// 2. Mudar atributos (Imagens)
+function imagem1() {
+    document.getElementById("myImage").src = "boneco.jpg";
+}
+
+function imagem2() {
+    document.getElementById("html.jpg")
+    document.getElementById("myImage").src = "html.jpg"; 
+}
+
+// 3. Mudar estilos CSS (Tamanho e Cor)
+function estiloNormal() {
+    document.getElementById("demo2").style.fontSize = "16px";
+    document.getElementById("demo2").style.color = "black";
+}
+
+function estiloGrande() {
+    document.getElementById("demo2").style.fontSize = "35px";
+    document.getElementById("demo2").style.color = "red";
+}
+
+// 4. Esconder e Mostrar elementos
+function esconder() {
+    document.getElementById("demo3").style.display = "none";
+}
+
+function mostrar() {
+    document.getElementById("demo3").style.display = "block";
+}
